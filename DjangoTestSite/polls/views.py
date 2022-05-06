08672bc8d64c8ffb32520769cb6b66ce47854641
Brent Williams <brent.williams333@gmail.com>
2022-05-06 11:58:20 -0700

    Fix recent bug, add method and UX tests

--- DjangoTestSite/polls/views.py
+++ DjangoTestSite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -11,8 +12,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published question"""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions that are currently active"""
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
